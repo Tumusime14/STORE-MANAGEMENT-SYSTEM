75e7e325c9dad82cd8d9c997a2d4e0d6aab48cb2
Tumusime14 <109425376+Tumusime14@users.noreply.github.com>
2023-03-04 12:13:13 -0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Choose what you're going to with system");
+    }
+}
---
+++ Customer.java
@@ -0,0 +1,44 @@
+public class Customer {
+    public int customerId;
+    public String customerName;
+    public String address;
+    public String phone;
+
+    public Customer(int customerId, String customerName) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
