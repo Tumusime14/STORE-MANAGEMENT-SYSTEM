04685921424efbceff79d749ad70c2fff77bbf45
Tumusime14 <tumusimedavid14@gmail.com>
2023-03-09 01:46:32 +0200

    E

--- Transaction.java
+++ Transaction.java
@@ -15,10 +15,7 @@ public class Transaction extends Product {
     public Transaction(int productId, int shopId, String productName, int quantity) {
         super(productId, productName);
     }
-    public void Purchase() {
-        double price;
-        int quantity;
-    }
+
     public double getPrice() {
         return price;
     }
@@ -34,4 +31,25 @@ public class Transaction extends Product {
         System.out.println(quantity);
     
 }
+void purchasee(){
+    public double calculateTotal(Product[] products) {
+        double total = 0.0;
+        for (Product product : products) {
+            total += product.getPrice();
+        }
+        return total;
+    }
+    public String generateReceipt(Product[] products, double total, PaymentMethod paymentMethod) {
+        StringBuilder receipt = new StringBuilder();
+        receipt.append("Transaction Receipt:\n\n");
+        for (Product product : products) {
+            receipt.append(product.getPrice() + " - $" + product.getPrice() + "\n");
+        }
+        receipt.append("\nTotal: $" + total + "\n");
+        receipt.append("Payment Method: " + paymentMethod + "\n");
+        receipt.append("Thank you for your purchase!\n");
+        return receipt.toString();
+    }
+}
+
 }
